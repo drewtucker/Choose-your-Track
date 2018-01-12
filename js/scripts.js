956a70ad2ebcc09b2eb0308c20a5a2fc54559742
Drew Tucker <dtuck43@gmail.com>
2018-01-12 11:16:14 -0800

    Finish front-end forms, suggest learning CSS or Design based on user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
   $("form#frontOrBack").submit(function(event) {
-    var input = $("input:radio[name=frontOrBack]:checked").val();
+    var userInput = $("input:radio[name=frontOrBack]:checked").val();
     event.preventDefault();
-
+    if (userInput === "frontEnd") {
+      $("form#selectFrontEnd").show();
+      $("form#frontOrBack").hide();
+    }
+    else if (userInput === "backEnd") {
+      $("form#selectBackEnd").show();
+      $("form#frontOrBack").hide();
+    }
   });
 
+  $("form#selectFrontEnd").submit(function(event) {
+    var userInput = $("input:radio[name=selectFrontEnd]:checked").val();
+    event.preventDefault();
+    if (userInput === "specific") {
+      $("#selectSpecific").show();
+      $("form#selectFrontEnd").hide();
+    }
+    else if (userInput === "broad") {
+      $("#selectBroad").show();
+      $("form#selectFrontEnd").hide()
+    }
+});
 });
